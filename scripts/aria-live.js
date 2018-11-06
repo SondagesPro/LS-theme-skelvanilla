9e43b477da23c0e875b9551f7518e8237ee4895a
Denis Chenu <denis@sondages.pro>
2018-11-06 13:29:16 +0100

    [feature] Better separation between each help part

--- scripts/aria-live.js
+++ scripts/aria-live.js
@@ -3,19 +3,23 @@ var TemplateAccessible = {
         this.triggerEmClassChangeAccessible();
         this.triggerMandatoryUpdate();
         this.triggerRelevanceOnOff();
+        this.triggerHtmlUpdated();
     },
     triggerEmClassChangeAccessible: function () {
         $(document).on('classChangeError','.ls-em-tip', function () {
             if($(this).attr("role") != "alert") {
-                parent = $(this).parent('.ls-question-help');
-                $(this).closest(".question-container").find("[aria-describedby*='ls-question-help-']").attr("aria-invalid",true);
+                parentId = $(this).parent('.ls-question-help').attr("id");
+                $("[aria-describedby*='"+parentId+"']").attr("aria-invalid",true);
+                $("[aria-labelledby*='"+parentId+"']").attr("aria-invalid",true);
                 $(this).attr("role","alert");
             }
         });
        $(document).on('classChangeGood','.ls-em-tip', function () {
             $(this).removeAttr("role");
             if($(this).parent('.ls-question-help').find(".ls-em-error").length == 0) {
-                $(this).closest(".question-container").find("[aria-describedby*='ls-question-help-']").removeAttr("aria-invalid");
+                parentId = $(this).parent('.ls-question-help').attr("id");
+                $("[aria-describedby*='"+parentId+"']").removeAttr("aria-invalid");
+                $("[aria-labelledby*='"+parentId+"']").removeAttr("aria-invalid");
             }
         });
     },
@@ -23,6 +27,9 @@ var TemplateAccessible = {
         /* @todo #1 add (and remove) required to dropdown, input:text, input:radio and textarea according to relevance */
         /* @todo #2 dynamic "aria-invalid" on group for checkbox (multiple choice and array-number / checkbox */
         /* @todo #3 dynamic "aria-invalid" on dropdown, input:text and textarea */
+    },
+    triggerHtmlUpdated : function() {
+        /* @todo use html:updated event to trigger aria-live */
     },
     triggerRelevanceOnOff: function() {
         this.triggerRelevanceOnOffQuestion();
