d33e4d6e0c06b7f656364a19e9557c6eaea6e17d
Denis Chenu <denis@sondages.pro>
2018-12-04 15:57:02 +0100

    [doc] Just information

--- scripts/skelvanilla.js
+++ scripts/skelvanilla.js
@@ -1,30 +1,7 @@
 /*
-    LimeSurvey
-    Copyright (C) 2007-2016 The LimeSurvey Project Team / Carsten Schmitz
-    All rights reserved.
-    License: GNU/GPL License v3 or later, see LICENSE.php
-    LimeSurvey is free software. This version may have been modified pursuant
-    to the GNU General Public License, and as distributed it includes or
-    is derivative of works licensed under the GNU General Public License or
-    other free or open source software licenses.
-    See COPYRIGHT.php for copyright notices and details.
-
-
-    (¯`·._.·(¯`·._.·  Theme main JavaScript   ·._.·´¯)·._.·´¯)
-
-     Javascript file for this template.
-
-     You'd rather not touch it. This file can be overwritten by an update.
-
-
-     ██████╗  ██████╗     ███╗   ██╗ ██████╗ ████████╗    ████████╗ ██████╗ ██╗   ██╗ ██████╗██╗  ██╗    ██╗
-     ██╔══██╗██╔═══██╗    ████╗  ██║██╔═══██╗╚══██╔══╝    ╚══██╔══╝██╔═══██╗██║   ██║██╔════╝██║  ██║    ██║
-     ██║  ██║██║   ██║    ██╔██╗ ██║██║   ██║   ██║          ██║   ██║   ██║██║   ██║██║     ███████║    ██║
-     ██║  ██║██║   ██║    ██║╚██╗██║██║   ██║   ██║          ██║   ██║   ██║██║   ██║██║     ██╔══██║    ╚═╝
-     ██████╔╝╚██████╔╝    ██║ ╚████║╚██████╔╝   ██║          ██║   ╚██████╔╝╚██████╔╝╚██████╗██║  ██║    ██╗
-     ╚═════╝  ╚═════╝     ╚═╝  ╚═══╝ ╚═════╝    ╚═╝          ╚═╝    ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝    ╚═╝
-
-     Please, use custom.js
+    This a copy of Vanilla theme.js filoe with some update for skelvanilla
+    Broke dependency of Vanilla template
+    Please, use theme.js or custom.js
 
 */
 
